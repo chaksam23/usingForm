cce7b496accc7f6b3517733054b671434894d1c8
sampurna <sampurnachakraborty798@gmail.com>
2025-07-05 12:52:23 +0530

    db connection done

---
+++ db_connect.php
@@ -0,0 +1,15 @@
+<?php
+    $host_name="localhost";
+    $user_name="root";
+    $password="root";
+    $dbname="school_management";
+
+    //Create connection
+    $conn = mysqli_connect($host_name, $user_name, $password, $dbname);
+    // if($conn){
+    //     die('Successful!');
+    // } else {
+    //    die('Db Error!'); 
+    // }
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
+<?php include 'db_connect.php';?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
